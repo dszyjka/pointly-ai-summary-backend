9f4bf199d691dd9a0705dbb943de69681cf7e0d7
Dawid Szyjka <dawid.szyjkaa1910@interia.pl>
2026-05-12 22:03:19 +0200

    feat: implement reading user uuid from header

--- backend/routers/summarize.py
+++ backend/routers/summarize.py
@@ -1,4 +1,4 @@
-from fastapi import File, UploadFile, Form, Depends
+from fastapi import File, UploadFile, Form, Depends, Header, HTTPException
 from fastapi.responses import StreamingResponse
 from fastapi import APIRouter
 from typing import Annotated
@@ -10,12 +10,17 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 router = APIRouter()
 
+async def get_user_from_header(x_user_id: str = Header(None)):
+    if x_user_id is None:
+        raise HTTPException(400, detail='Missing X-USER-ID header')
+    return x_user_id
+
 @router.post('/summarize') # multipart/form-data
 async def post_summarize(
                 file: Annotated[UploadFile, File()],
                 response_type: Annotated[ResponseType, Form()],
                 user_rules: Annotated[list[str], Form()],
-                user_id: Annotated[str, Form()],
+                user_id: str = Depends(get_user_from_header),
                 db: AsyncSession = Depends(get_db),
                     ):
 
