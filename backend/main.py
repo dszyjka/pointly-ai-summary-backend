2ab6370acff97aadd1408b6d43f394b7f770e592
Dawid Szyjka <dawid.szyjkaa1910@interia.pl>
2026-05-13 23:36:42 +0200

    feat: add CORS Middleware

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from config import settings
 from .database.database import init_db
 from .routers import summarize, history
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
 
 
 @asynccontextmanager
@@ -13,3 +14,11 @@ async def lifespan(app: FastAPI):
 app = FastAPI(lifespan=lifespan)
 app.include_router(summarize.router, prefix='/api', tags=['summarization'])
 app.include_router(history.router, prefix='/api', tags=['history'])
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Allows all origins (fine for development)
+    allow_credentials=True,
+    allow_methods=["*"], # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"], # Allows all headers (including your x-user-id)
+)
