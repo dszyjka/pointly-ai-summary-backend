8c1ab501064d6bda5337d91c373a7a7fa7767c53
Dawid Szyjka <dawid.szyjkaa1910@interia.pl>
2026-06-11 12:09:19 +0200

    fix: correct save_to_db

--- backend/database/database.py
+++ backend/database/database.py
@@ -2,14 +2,20 @@ from sqlmodel import SQLModel
 from config import settings
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
+is_sqlite = "sqlite" in settings.database_url
 
 engine = create_async_engine(
     settings.database_url,
     echo=True,
-    connect_args={'check_same_thread' : False} if 'sqlite' in settings.database_url else {})
+    connect_args={"check_same_thread": False} if is_sqlite else {}
+)
 
-async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
- 
+async_session = async_sessionmaker(
+    engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
@@ -18,6 +24,8 @@ async def get_db():
     async with async_session() as session:
         yield session
 
-async def save_to_db(db, record):
+async def save_to_db(db: AsyncSession, record):
     db.add(record)
     await db.commit()
+    await db.refresh(record)
+    return record
