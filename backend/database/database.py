ab577fb278fab6cbe3f8edc4f07a41ddf9276ac5
Dawid Szyjka <dawid.szyjkaa1910@interia.pl>
2026-06-11 14:19:29 +0200

    refactor: modify engine create func

--- backend/database/database.py
+++ backend/database/database.py
@@ -2,20 +2,14 @@ from sqlmodel import SQLModel
 from config import settings
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 
-is_sqlite = "sqlite" in settings.database_url
 
 engine = create_async_engine(
     settings.database_url,
     echo=True,
-    connect_args={"check_same_thread": False} if is_sqlite else {}
-)
+    connect_args={'check_same_thread' : False if 'sqlite' in settings.database_url else {}})
 
-async_session = async_sessionmaker(
-    engine,
-    class_=AsyncSession,
-    expire_on_commit=False
-)
-
+async_session = async_sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
+ 
 async def init_db():
     async with engine.begin() as conn:
         await conn.run_sync(SQLModel.metadata.create_all)
